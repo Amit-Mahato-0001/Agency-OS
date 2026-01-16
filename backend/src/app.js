c9763c801c7a305137f725e3d6ca3de25c6cb624
Amit-Mahato-0001 <amitmahatocky909@gmail.com>
2026-01-16 22:03:08 +0530

    auth middleware

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const express = require('express')
 const connectDB = require('./config/db')
 const router = require('./routes/auth.route')
+const authenticate = require('./middleware/auth.middleware')
 
 const app = express()
 connectDB()
@@ -12,6 +13,17 @@ app.get('/health', (req, res) => {
     res.json({ status: "Ok hai ji" })
 })
 
-app.use('/api/auth', router)
+app.use(authenticate)
+
+app.get("/api/me", (req, res) => {
+    res.json({
+        user: req.user,
+        tenant: {
+            id: req.tenant._id,
+            name: req.tenant.name,
+            plan: req.tenant.plan
+        }
+    })
+})
 
 module.exports = app
